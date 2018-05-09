81211545c26d3ca1ddb844d9e8971a844fb2d3a2
flyfxj <flyfxj@yahoo.com>
2018-05-09 02:05:16 -0400

    update file

--- src/container.py
+++ src/container.py
@@ -13,6 +13,8 @@ Basic operation as follow
 3. get a data(for queue should be pop action)
 4. clean(clean all data)
 '''
+import util
+import threading
 
 class container(object):
     """docstring for container"""
@@ -39,10 +41,49 @@ class queue(container):
     def __init__(self):
         super(queue, self).__init__()
         self.name = 'Queue Container'
+        self._content = []
+
+    def addData(self, *args, **kwargs):
+        '''To add a data into container'''
+        super(queue, self).addData()
+        for arg in args:
+            self._content.append(arg)
+        
+    def getData(self, *args, **kwargs):
+        '''To get a data from container'''
+        super(queue, self).getData()
+        return self._content.pop() if self._content else None
+
+    def resetData(self, *args, **kwargs):
+        '''To reset a data into container'''
+        super(queue, self).resetData()
+        self._content = []
+
+@util.SingletonCls
+class webContentQueue(queue):
+    def __init__(self, *args, **kwargs):
+        #1.one possible super class call method
+        #queue.__init__(self)
+        #2.another possible super class call method
+        super(kwargs['cls'], self).__init__()
+        #This way will faled because wrapper will do webContentQueue = SingletonCls(webContentQueue)
+        #so the webContentQueue have been change to SingletonCls, no longer webContentQueue. 
+        self.name = 'webContentQueue Container'
+
+
 
 def containerTest(container):
-    container.addData()
-    container.getData()
+    container.addData("Test")
+    msg = container.getData()
+    if msg is not None:
+        print(msg)
+    else:
+        print("Msg is None")
+    msg = container.getData()
+    if msg is not None:
+        print(msg)
+    else:
+        print("Msg is None")
     container.resetData()
 
 if __name__ == '__main__':
@@ -50,3 +91,5 @@ if __name__ == '__main__':
     containerTest(c)
     q = queue()
     containerTest(q)
+    w = webContentQueue()
+    containerTest(w)
--- src/util.py
+++ src/util.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import threading
 
 def singletonFunc(cls, *args, **kwargs):
     '''one of singleton usage definition by closure'''
@@ -12,6 +13,17 @@ def singletonFunc(cls, *args, **kwargs):
 
 class SingletonCls(object):
     '''one of singleton usage definition by class'''
+    def __init__(self, cls):
+        self.cls = cls
+        self._instances = None
+    def __call__(self, *args, **kwargs):
+        if not self._instances:
+            kwargs['cls'] = self.cls
+            self._instances = self.cls(*args, **kwargs)
+        return self._instances
+
+class SngltnClsThrdSf(object):
+    '''one of thread safe singleton usage definition by class'''
     def __init__(self, cls):
         self.cls = cls
         self._instances = None
